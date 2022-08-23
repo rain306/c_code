9841b05486da6bd5f393d635b5a5c033bf36d49c
rain306 <111834664+rain306@users.noreply.github.com>
2022-08-23 14:55:14 +0800

    1

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//include 包含一个文件： stdio.h
+//其中std-标准  standard input output 标准输入输出
+int main()//主函数，程序入口，有且只有一个
+{
+	printf("hello\n");
+	return 0;
+}
+//int-整型  int main 表示调用main函数并返回一个整型值
+//return 0表示返回一个整型值-0
+//打印函数-print function-简写：printf
+//---/n表示换行
