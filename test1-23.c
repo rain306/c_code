649c4e00b04e97fbe44b4c39ac29ae4c8e47fc9a
rain306 <111834664+rain306@users.noreply.github.com>
2022-09-11 17:19:25 +0800

    数组

---
+++ test1-23.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	/*char arr1[] = "abc";
+	int sz = sizeof(arr1) / sizeof(arr1[0]);
+	printf("%d\n", sz);
+	int i;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%c\n", arr1[i]);
+	}*/
+	int arr[3][4] = { {1,2,3},{4,5,6},{7,8,9} };//¶þÎ¬Êý×é
+	printf("");
+	
+		
+
+
+	return 0;
+}
