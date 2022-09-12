f457a447195b65ac7f1f9631c693d477c709a96c
rain306 <111834664+rain306@users.noreply.github.com>
2022-09-12 16:58:02 +0800

    1

---
+++ test1-9.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	//int a = 10;
+	//int b = a++;//a赋给b后自身加一
+	//printf("a=%d\n", a);
+	//printf("b=%d\n", b);
+
+	//int c = ++a;//a自身加一后赋给c
+	//printf("a=%d\n", a);
+	//printf("c=%d\n", c);
+	int a = (int)3.14;
+	printf("a=%d\n", a);
+
+	/*int a = 1;
+	int b = ~a;*/
+
+
+
+
+
+
+	return 0;
+}
